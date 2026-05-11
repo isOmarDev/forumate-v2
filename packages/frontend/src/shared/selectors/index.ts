85699c6c0836abf1acf39b70b2fce07c0cfa80ca
omar kamel <isomardev@gmail.com>
2026-05-11 18:59:58 +0300

    feat: add appSelectors and utility functions for element selection in the frontend

---
+++ packages/frontend/src/shared/selectors/index.ts
@@ -0,0 +1,45 @@
+type ElementType = keyof JSX.IntrinsicElements;
+
+export type PageElementsSelector<T extends ElementType = ElementType> = {
+  selector: string;
+  type: T;
+};
+
+const el = <T extends ElementType>(
+  selector: string,
+  type: T,
+): PageElementsSelector<T> => ({
+  selector,
+  type,
+});
+
+export const appSelectors = {
+  registration: {
+    registrationForm: {
+      form: el('.registration-form', 'form'),
+      email: el('.registration.email', 'input'),
+      username: el('.registration.username', 'input'),
+      firstname: el('.registration.first-name', 'input'),
+      lastname: el('.registration.last-name', 'input'),
+      marketingCheckbox: el('.registration.marketing-emails', 'input'),
+      submit: el('.registration.submit-button', 'button'),
+    },
+  },
+  layout: { header: el('.header.username', 'div') },
+  notifications: {
+    failure: el('#failure-toast', 'div'),
+    success: el('#success-toast', 'div'),
+  },
+} as const;
+
+export const toClass = (input: string): string => {
+  return input.slice(1).replace(/\./g, ' ');
+};
+
+export const toId = (input: string): string => {
+  if (!input.startsWith('#')) {
+    throw new Error('Input string must start with a hash symbol (#).');
+  }
+
+  return input.slice(1);
+};
