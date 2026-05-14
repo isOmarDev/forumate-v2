c854519460c2c74fa3ea60b31df03b4d65ac9b43
omar kamel <isomardev@gmail.com>
2026-05-15 01:44:13 +0300

    fix: move PostRepo import to the correct adapters directory

--- packages/backend/src/modules/post/post-module.ts
+++ packages/backend/src/modules/post/post-module.ts
@@ -1,7 +1,7 @@
-import { PostRepo } from './post-repo';
 import { PostService } from './post-service';
 import { PostController } from './post-controller';
 import { PostErrors } from './post-errors';
+import { PostRepo } from './adapters/post-repo';
 import WebServer from '../../shared/server';
 import { Database } from '../../shared/database';
 
