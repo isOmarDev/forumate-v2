20079109d2783015f0ee7e0da2fe1702595b2210
omar kamel <isomardev@gmail.com>
2026-05-16 19:27:09 +0300

    refactor: decouple UserService and integrate ITransactionalEmailAPI

--- packages/backend/src/modules/user/user-service.ts
+++ packages/backend/src/modules/user/user-service.ts
@@ -4,33 +4,42 @@ import {
   EmailAlreadyInUseException,
   UsernameAlreadyTakenException,
 } from './user-exceptions';
+import { ITransactionalEmailAPI } from '../notification/ports/transactional-email-api';
 
 export class UserService {
-  constructor(private userRepo: IUserRepository) {}
+  constructor(
+    private repo: IUserRepository,
+    private emailApi: ITransactionalEmailAPI,
+  ) {}
 
   public async createUser(dto: CreateUserCommand) {
-    const existingUserByEmail = await this.userRepo.findByEmail(dto.email);
+    const existingUserByEmail = await this.repo.findByEmail(dto.email);
 
     if (existingUserByEmail) {
       throw new EmailAlreadyInUseException();
     }
 
-    const existingUserByUsername = await this.userRepo.findByUsername(
-      dto.username,
-    );
+    const existingUserByUsername = await this.repo.findByUsername(dto.username);
 
     if (existingUserByUsername) {
       throw new UsernameAlreadyTakenException();
     }
 
-    const { password, ...user } = await this.userRepo.create(
+    const { password, ...user } = await this.repo.create(
       dto.props as CreateUserCommand,
     );
+
+    await this.emailApi.sendMail({
+      to: user.email,
+      subject: `Welcome ${user.firstName} to our forum!`,
+      text: 'We are honored to have you in our forum, please feel free to explore it.',
+    });
+
     return user;
   }
 
   public async getUsers(filters?: { email?: string }) {
-    const users = await this.userRepo.findAll(filters);
+    const users = await this.repo.findAll(filters);
     return users.map((user) => {
       const { password, ...restUser } = user;
       return restUser;
