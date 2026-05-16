7266e0704f2b3569030b5d65ed945ac510349a06
omar kamel <isomardev@gmail.com>
2026-05-16 19:25:26 +0300

    refactor: decouple MarketingService by removing FakeMailService and integrating IContactListApi

--- packages/backend/src/modules/marketing/marketing-service.ts
+++ packages/backend/src/modules/marketing/marketing-service.ts
@@ -1,14 +1,14 @@
-import { FakeMailService } from '../notification/fake-mail-service';
 import { AddEmailToListDTO } from './marketing-dto';
 import { EmailNotAddedToMailListException } from './marketing-exceptions';
+import { IContactListApi } from './ports/contact-list-api';
 
 export class MarketingService {
-  constructor(private fakeMailService: FakeMailService) {}
+  constructor(private contactListAPI: IContactListApi) {}
 
   public async addEmailToList(dto: AddEmailToListDTO) {
     const { email } = dto;
 
-    const result = this.fakeMailService.addEmailToList(email);
+    const result = this.contactListAPI.addEmailToList(email);
 
     if (!result) {
       throw new EmailNotAddedToMailListException(email);
