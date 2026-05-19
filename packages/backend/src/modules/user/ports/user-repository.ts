95293081051e080448fea1b9b1c4d2d14184d080
omar kamel <isomardev@gmail.com>
2026-05-19 23:52:16 +0300

    feat: update create method to use CreateUserInput for user creation

--- packages/backend/src/modules/user/ports/user-repository.ts
+++ packages/backend/src/modules/user/ports/user-repository.ts
@@ -1,8 +1,8 @@
 import { User } from '../../../shared/database';
-import { CreateUserCommand } from '../user-command';
+import { CreateUserInput } from '@dddforum/shared/api/users';
 
 export interface IUserRepository {
-  create(user: CreateUserCommand): Promise<User>;
+  create(user: CreateUserInput): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
   findByUsername(username: string): Promise<User | null>;
   findAll(filters?: { email?: string }): Promise<User[]>;
