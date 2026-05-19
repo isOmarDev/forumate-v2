2f2af195a16ce91c1a068b3e9cb65d474db7a9bd
omar kamel <isomardev@gmail.com>
2026-05-19 23:53:07 +0300

    feat: make builder methods public in CreateUserInputBuilder class

--- packages/shared/tests/support/builders/create-user-input-builder.ts
+++ packages/shared/tests/support/builders/create-user-input-builder.ts
@@ -15,27 +15,27 @@ export class CreateUserInputBuilder {
     };
   }
 
-  withEmail(email: string = faker.internet.email()) {
+  public withEmail(email: string = faker.internet.email()) {
     this.props.email = email;
     return this;
   }
 
-  withUsername(username: string = faker.internet.username()) {
+  public withUsername(username: string = faker.internet.username()) {
     this.props.username = username;
     return this;
   }
 
-  withFirstName(firstName: string = faker.person.firstName()) {
+  public withFirstName(firstName: string = faker.person.firstName()) {
     this.props.firstName = firstName;
     return this;
   }
 
-  withLastName(lastName: string = faker.person.lastName()) {
+  public withLastName(lastName: string = faker.person.lastName()) {
     this.props.lastName = lastName;
     return this;
   }
 
-  withPassword(
+  public withPassword(
     password?: string,
     options?: Parameters<typeof faker.internet.password>[0],
   ) {
