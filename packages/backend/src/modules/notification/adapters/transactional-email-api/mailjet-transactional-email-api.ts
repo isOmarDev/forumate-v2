6b3129d29fd81810ddff0728100c622ea86f03b1
omar kamel <isomardev@gmail.com>
2026-05-21 19:26:27 +0300

    fix: correct casing of ITransactionalEmailApi and update log message format

--- packages/backend/src/modules/notification/adapters/transactional-email-api/mailjet-transactional-email-api.ts
+++ packages/backend/src/modules/notification/adapters/transactional-email-api/mailjet-transactional-email-api.ts
@@ -1,13 +1,13 @@
 import {
-  ITransactionalEmailAPI,
+  ITransactionalEmailApi,
   SendMailInput,
   SendMailResult,
 } from '../../ports/transactional-email-api';
 
-export class MailjetTransactionalEmailAPI implements ITransactionalEmailAPI {
+export class MailjetTransactionalEmailApi implements ITransactionalEmailApi {
   async sendMail(input: SendMailInput): Promise<SendMailResult> {
     console.log(
-      `Sending email to ${input.to} with subject ${input.subject} and text ${input.text}`,
+      `[MailJet] Sending email to ${input.to} with subject ${input.subject} and text ${input.text}`,
     );
 
     new Promise((resolve) => setTimeout(resolve, 500));
