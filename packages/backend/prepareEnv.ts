7a5d3859ae0a9cbb15310b3d92e816c601cc9f5b
omar kamel <isomardev@gmail.com>
2026-06-07 21:43:27 +0300

    feat: add prepareEnv function to manage environment setup for different modes

---
+++ packages/backend/prepareEnv.ts
@@ -0,0 +1,27 @@
+import { execSync } from 'child_process';
+import * as path from 'path';
+
+export const prepareEnv = (): void => {
+  const env = process.env.NODE_ENV || 'development';
+  const packageRoot = path.resolve(__dirname);
+  const execParams = {
+    cwd: packageRoot,
+    stdio: 'inherit',
+  } as const;
+
+  const script = process.argv.splice(2).join(' ');
+
+  if (env === 'development') {
+    const devEnvFile = '.env.development';
+    console.log(`Preparing dev environment using ${devEnvFile}`);
+    execSync(`dotenv -e ${devEnvFile} -- ${script}`, execParams);
+    return;
+  }
+
+  console.log(
+    `Running ${script} in ${process.env.NODE_ENV} mode without loading from env file.`,
+  );
+  execSync(`${script}`, execParams);
+};
+
+prepareEnv();
