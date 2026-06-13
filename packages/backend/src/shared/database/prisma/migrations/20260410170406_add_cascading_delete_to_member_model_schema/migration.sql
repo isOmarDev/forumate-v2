448ef2e34edd9593d7ae1273b2e4fc01a2ca9cc1
omar kamel <isomardev@gmail.com>
2026-06-13 17:12:59 +0300

    refactor: remove outdated cascading delete migration and initialize database schema

--- packages/backend/src/shared/database/prisma/migrations/20260410170406_add_cascading_delete_to_member_model_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Member" DROP CONSTRAINT "Member_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
