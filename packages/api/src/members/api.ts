804c8c00695bfbbb5b0d381923ee7df000530e8c
omar kamel <isomardev@gmail.com>
2026-08-25 22:41:25 +0300

    refactor: specify API response types for createMembersApi methods for consistency

--- packages/api/src/members/api.ts
+++ packages/api/src/members/api.ts
@@ -12,10 +12,19 @@ export const createMembersApi = (client: HttpClient) => {
   return {
     register: (input: CreateMemberInput, authToken: string) =>
       apiRequest(() =>
-        client.post('/members', input, getAuthHeaders(authToken)),
+        client.post<CreateMemberApiResponse>(
+          '/members',
+          input,
+          getAuthHeaders(authToken),
+        ),
       ),
 
     getMemberDetails: (authToken: string) =>
-      apiRequest(() => client.get('/members/me', getAuthHeaders(authToken))),
+      apiRequest(() =>
+        client.get<GetMemberDetailsApiResponse>(
+          '/members/me',
+          getAuthHeaders(authToken),
+        ),
+      ),
   };
 };
