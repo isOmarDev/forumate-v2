e0aeb87af30d8b18a522207f80b27c369160fdd8
omar kamel <isomardev@gmail.com>
2026-08-27 00:06:41 +0300

    refactor: standardize file naming conventions to kebab case

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,23 +1,18 @@
 // Application exports
 export { Collection } from './application/collection';
-export { type IHandle } from './application/eventHandler';
-export { ReadModel } from './application/readModel';
+export { type IHandle } from './application/event-handler';
+export { ReadModel } from './application/read-model';
 export * from './application/request';
-export {
-  fail,
-  Result,
-  success,
-  type UseCase,
-  type UseCaseResponse,
-} from './application/useCase';
+export { type UseCase } from './application/use-case';
+export { Result, success, fail } from './application/result';
 
 // Domain exports
-export { AggregateRoot } from './domain/aggregateRoot';
-export { DomainEvent, type DomainEventStatus } from './domain/domainEvent';
-export { type EventModel } from './domain/eventModel';
-export { ValueObject } from './domain/valueObject';
+export { AggregateRoot } from './domain/aggregate-root';
+export { DomainEvent, type DomainEventStatus } from './domain/domain-event';
+export { type EventModel } from './domain/event-model';
+export { ValueObject } from './domain/value-object';
 
 // Utils exports
-export { DateUtil } from './utils/dateUtil';
-export { NumberUtil } from './utils/numberUtil';
-export { TextUtil } from './utils/textUtil';
+export { DateUtil } from './utils/date-util';
+export { NumberUtil } from './utils/number-util';
+export { TextUtil } from './utils/text-util';
