8a86377041ad0a7d2be044e60f3aeeb25a62e0c1
omar kamel <isomardev@gmail.com>
2026-08-27 22:11:50 +0300

    refactor: simplify index exports by consolidating application, domain, and utils modules

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,18 +1,3 @@
-// Application exports
-export { Collection } from './application/collection';
-export { type IHandle } from './application/event-handler';
-export { ReadModel } from './application/read-model';
-export * from './application/request';
-export { type UseCase } from './application/use-case';
-export { Result, success, fail } from './application/result';
-
-// Domain exports
-export { AggregateRoot } from './domain/aggregate-root';
-export { DomainEvent, type DomainEventStatus } from './domain/domain-event';
-export { type EventModel } from './domain/event-model';
-export { ValueObject } from './domain/value-object';
-
-// Utils exports
-export { DateUtil } from './utils/date-util';
-export { NumberUtil } from './utils/number-util';
-export { TextUtil } from './utils/text-util';
+export * from './application';
+export * from './domain';
+export * from './utils';
