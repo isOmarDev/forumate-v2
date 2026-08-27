a880bff40f7e0e2cdabf950bb261b89257086aab
omar kamel <isomardev@gmail.com>
2026-08-27 22:12:46 +0300

    refactor: update imports to use 'type' for type-only imports and improve error handling in commands

--- packages/api/src/votes/commands.ts
+++ packages/api/src/votes/commands.ts
@@ -1,8 +1,9 @@
-import { Request } from '@forumate/core';
-import { MissingRequestParamsError } from '@forumate/errors/server';
+import { type Request } from '@forumate/core/application';
+import { InvalidRequestBodyError } from '@forumate/errors/request';
 
-import { VoteOnCommentInput, VoteOnPostInput } from './inputs';
+import type { VoteOnCommentInput, VoteOnPostInput } from './inputs';
 
+// Update Member Reputation
 export class UpdateMemberReputationScoreCommand {
   constructor(
     public readonly props: {
@@ -11,46 +12,60 @@ export class UpdateMemberReputationScoreCommand {
   ) {}
 }
 
+// Vote On Comment
 export class VoteOnCommentCommand {
-  constructor(public props: VoteOnCommentInput) {}
+  constructor(public readonly props: VoteOnCommentInput) {}
 
-  static fromRequest(body: Request['body']) {
+  static fromRequest(
+    body: Request<VoteOnCommentInput>['body'],
+  ): VoteOnCommentCommand {
     const { voteType, commentId, memberId } = body;
 
     if (!commentId) {
-      throw new MissingRequestParamsError(['commentId']);
+      throw new InvalidRequestBodyError(['commentId']);
     }
 
     if (!voteType) {
-      throw new MissingRequestParamsError(['voteType']);
+      throw new InvalidRequestBodyError(['voteType']);
     }
 
     if (!memberId) {
-      throw new MissingRequestParamsError(['memberId']);
+      throw new InvalidRequestBodyError(['memberId']);
     }
 
-    return new VoteOnCommentCommand({ ...body });
+    return new VoteOnCommentCommand({
+      commentId,
+      voteType,
+      memberId,
+    });
   }
 }
 
+// Vote On Post
 export class VoteOnPostCommand {
-  constructor(public props: VoteOnPostInput) {}
+  constructor(public readonly props: VoteOnPostInput) {}
 
-  static fromRequest(body: Request['body']) {
+  static fromRequest(
+    body: Request<VoteOnPostInput>['body'],
+  ): VoteOnPostCommand {
     const { voteType, postId, memberId } = body;
 
     if (!postId) {
-      throw new MissingRequestParamsError(['postId']);
+      throw new InvalidRequestBodyError(['postId']);
     }
 
     if (!voteType) {
-      throw new MissingRequestParamsError(['voteType']);
+      throw new InvalidRequestBodyError(['voteType']);
     }
 
     if (!memberId) {
-      throw new MissingRequestParamsError(['memberId']);
+      throw new InvalidRequestBodyError(['memberId']);
     }
 
-    return new VoteOnCommentCommand({ ...body });
+    return new VoteOnPostCommand({
+      postId,
+      voteType,
+      memberId,
+    });
   }
 }
--- packages/api/src/votes/inputs.ts
+++ packages/api/src/votes/inputs.ts
@@ -1,7 +1,5 @@
 export type VoteType = 'upvote' | 'downvote';
 
-// Inputs
-// TODO: separate all inputs from commands; they're different - do this for all domains
 export type VoteOnCommentInput = {
   commentId: string;
   voteType: VoteType;
