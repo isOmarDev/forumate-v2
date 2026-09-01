45507dcde456ea038974a61a7d70fc4debb2465c
omar kamel <isomardev@gmail.com>
2026-09-01 21:44:43 +0300

    refactor: update import statement for IVoteRepository to use type keyword

--- apps/backend/src/modules/votes/application/use-cases/update-member-reputation/update-member-reputation-score.ts
+++ apps/backend/src/modules/votes/application/use-cases/update-member-reputation/update-member-reputation-score.ts
@@ -7,7 +7,7 @@ import { DatabaseError } from '@forumate/errors/server';
 import { Member } from '../../../../members/domain/entities/member';
 import { MemberNotFoundError } from '../../../../members/member-errors';
 import { type IMembersRepository } from '../../../../members/repos/ports/members-repository';
-import { IVoteRepository } from '../../../../votes/repos/ports/vote-repository';
+import { type IVoteRepository } from '../../../../votes/repos/ports/vote-repository';
 
 type UpdateMemberReputationError = NotFoundError | DatabaseError;
 
